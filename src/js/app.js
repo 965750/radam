e4b86bd5fe86753d8baa909699f0bc8c1d43e20f
965750 <aventhelast@gmail.com>
2018-02-15 12:45:09 +0100

    vue first

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,6 @@ new Vue({
         }, 
     },
     computed: {
-
+        
     }
 });
--- src/js/main.js
+++ src/js/main.js
@@ -9,14 +9,35 @@ $(window).on("scroll", function(){
             position: 'fixed',
             zIndex: 15,
             opacity: 0.8,
+            height: '40px'
             
+        });
+        $(".socialICont").css({
+            height: '25px',
+            width: '25px',
+            lineHeight: '27px'
+        });
+        $(".singleNavi").css({
+            height: '40px',
+            padding: '10px',
+            fontSize: '18px',
         });
     }else{
         $("#mainNav").css({
             position: '',
             zIndex: 0,
             opacity: 1,
-            height: '40px'
+            height: '70px'
+        });
+        $(".socialICont").css({
+            height: '35px',
+            width: '35px',
+            lineHeight: '37px'
+        });
+        $(".singleNavi").css({
+            height: '70px',
+            padding: '20px',
+            fontSize: '23px',
         });
     }
 });

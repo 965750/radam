f8dd622d91022ffa12f0fa0ae17cabd190d7c435
965750 <aventhelast@gmail.com>
2018-02-15 14:40:27 +0100

    vue second JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,27 @@
 new Vue({
     el: '#vue-app',
     data: {
-        
+        segzy: 'testoo',
+        products: [
+            {
+                name: 'Produkt 1',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quis optio, architecto necessitatibus, minus blanditiis perspiciatis totam nam quia facere sit unde maiores laboriosam incidunt beatae fuga ex? Similique repudiandae vero unde fugiat error dignissimos hic quia doloribus, est eos ea, nostrum maxime consequuntur enim temporibus officia rem velit.Ratione!',
+                cost: 129,
+                img: 'img/apple.jpg'
+            },
+            {
+                name: 'Produkt 2',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quis optio, architecto necessitatibus, minus blanditiis perspiciatis totam nam quia facere sit unde maiores laboriosam incidunt beatae fuga ex? Similique repudiandae vero unde fugiat error dignissimos hic quia doloribus, est eos ea, nostrum maxime consequuntur enim temporibus officia rem velit.Ratione!',
+                cost: 239,
+                img: 'img/pickle.jpg'
+            },
+            {
+                name: 'Produkt 2',
+                desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quis optio, architecto necessitatibus, minus blanditiis perspiciatis totam nam quia facere sit unde maiores laboriosam incidunt beatae fuga ex? Similique repudiandae vero unde fugiat error dignissimos hic quia doloribus, est eos ea, nostrum maxime consequuntur enim temporibus officia rem velit.Ratione!',
+                cost: 239,
+                img: 'img/pickle.jpg'
+            },
+        ],
     },
     methods: {
         readRefs: function () {

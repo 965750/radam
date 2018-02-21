b59509b5e78801dd2ca7bc7f743cd8dbbb4f6248
965750 <aventhelast@gmail.com>
2018-02-21 13:04:29 +0100

    form updates select

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ new Vue({
                 img: 'img/505.jpg'
             },
             {
-                name: 'Produkt 2',
+                name: 'Produkt 3',
                 desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quis optio, architecto necessitatibus, minus blanditiis perspiciatis totam nam quia facere sit unde maiores laboriosam incidunt beatae fuga ex? Similique repudiandae vero unde fugiat error dignissimos hic quia doloribus, est eos ea, nostrum maxime consequuntur enim temporibus officia rem velit.Ratione!',
                 cost: 239,
                 img: 'img/510_M.jpg'
@@ -27,21 +27,26 @@ new Vue({
         readRefs: function () {
 
         },
-        buyScroll: function () {
+        buyScroll: function (nameOfProduct) {
+           
+            //offset = windowY
             var ele = document.getElementById("orderForm");
-
             var bodyRect = document.body.getBoundingClientRect(),
                 elemRect = ele.getBoundingClientRect(),
                 offset = elemRect.top - bodyRect.top;
-                console.log(offset);
             //scrollTo
-
+            
             var timerID = setInterval(function () {
-                window.scrollBy(0, 10);
+                window.scrollBy(0, 12);
 
                 if (window.pageYOffset >= offset - 50)
                     clearInterval(timerID);
-            }, 10);
+            }, 5);
+            //change select
+            
+            var sel = document.getElementById('sel');
+            sel.value=nameOfProduct;
+
         },
     },
     computed: {
